ed75efb2487bbd3434515959211d4b86e60147c7
Pawan12397 <142645171+Pawan12397@users.noreply.github.com>
2023-08-21 09:35:37 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,12 @@
 import os
 
-class Config(object):
-    API_ID = int(os.environ.get("API_ID", "29877145"))
+class Config(object):    API_ID = int(os.environ.get("API_ID", "29877145"))
     API_HASH = os.environ.get("API_HASH", "85c320b142b7482589fff61da6370b07")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", 5825009313:AAHVCMjJ2vgqhpsRbPm2VzKDNfK9Lc1f5i8)
-    STRING_SESSION = os.environ.get("STRING_SESSION", 1BVtsOIEBu1lvpo3gzibKjrehHT7LGjoiBdkkw7iNd8YffdwAecC9IkGV2RNTeHDj71UEPHm8Vat09sZW0HGwqPUjUWBfmA0Pf4tIWkRcGjCUSsmQJlYOKpH4GPYWs_34M3_BDhLOz6YRPBs6Rain1juBJEJ8JWAvTCR1qPtLU6kjSBnWkG8aso5ha9JrOjSB36VK_2MXtwO5GlIjj4e_85W9J03rcXN_r1Z4BvoPJ5t5cIudYFnXXT4_5mk4LAwn_zUItbMkn_tVoXI8R11yaXIZHBbAoeZFni5qanMf2fUaNl9nKqZQyhHMDnzbdPvKaFJm0MaTuhhYHdNaJ4MBkyeH6h_QkJw=)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", 5825009313:
+    STRING_SESSION = os.environ.get("STRING_SESSION", 1
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", Biharisongmusic_bot"")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
