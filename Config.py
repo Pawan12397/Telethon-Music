f891803d4592a04a5bffe7110e3a121362288b38
Pawan12397 <142645171+Pawan12397@users.noreply.github.com>
2023-08-21 10:25:02 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,15 +2,15 @@ import os
 
 class Config(object):    API_ID = int(os.environ.get("API_ID", "29877145"))
     API_HASH = os.environ.get("API_HASH", "85c320b142b7482589fff61da6370b07")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", :
-    STRING_SESSION = os.environ.get("STRING_SESSION",
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None):
+    STRING_SESSION = os.environ.get(None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Biharisongmusic_bot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", None")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", 5999149323"")) # telegram I'd not Username
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", None)) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
     AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
